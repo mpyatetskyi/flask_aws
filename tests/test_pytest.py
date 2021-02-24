5e74a1632c6686ddb54d6ac6ae0cf3e72e265847
mpiatetskyi <m.pyatetskyi@gmail.com>
2021-02-24 18:32:02 +0200

    unit testing for routes

--- tests/test_pytest.py
+++ tests/test_pytest.py
@@ -1,39 +1,23 @@
 from main import app
 
 
-def test_index():
-    tester = app.test_client()
-    response = tester.get("/")
-    assert response.status_code == 200
-
-
-# Check if content is application/json
-def test_index_content():
-    tester = app.test_client()
-    response = tester.get("/", content_type='application/json')
-    assert response.content_type == 'application/json'
-
-
-# Check for data returned
-def test_index_data():
-    tester = app.test_client()
-    response = tester.get("/")
-    assert b'Hello World' in response.data
-
-
-def test_new_game_index():
-    tester = app.test_client()
-    response = tester.get("/newgame")
-    assert response.status_code == 200
-
-
-def test_new_game_content():
-    tester = app.test_client()
-    response = tester.get("/newgame", content_type='application/json')
-    assert response.content_type == 'application/json'
-
-
-def test_new_game():
+def test_status_code():
     tester = app.test_client()
     response = tester.get('/newgame')
-    assert response.json
+    assert response.status_code == 200
+    assert response.content_type == 'application/json'
+
+def test_game_id_in_response():
+    tester = app.test_client()
+    response = tester.get('/newgame')
+    assert b'game_id' in response.data
+
+def test_cards_in_response():
+    tester = app.test_client()
+    response = tester.get('/newgame')
+    assert b'cards' in response.data
+
+def test_cards_type_in_response():
+    tester = app.test_client()
+    response = tester.get('/newgame')
+    assert b'cards' in response.data
