dee766793f625d9282c6f808d2f6d6daef5d2959
mpiatetskyi <m.pyatetskyi@gmail.com>
2021-02-24 18:34:52 +0200

    unit testing for routes

--- tests/test_pytest.py
+++ tests/test_pytest.py
@@ -7,15 +7,18 @@ def test_status_code():
     assert response.status_code == 200
     assert response.content_type == 'application/json'
 
+
 def test_game_id_in_response():
     tester = app.test_client()
     response = tester.get('/newgame')
     assert b'game_id' in response.data
 
+
 def test_cards_in_response():
     tester = app.test_client()
     response = tester.get('/newgame')
     assert b'cards' in response.data
+
 
 def test_cards_type_in_response():
     tester = app.test_client()
